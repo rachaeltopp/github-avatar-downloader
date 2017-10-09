086a1a9c8d3c15ca4aeee6a2b398c7950d7146a4
Rachael Topp <topp@ualberta.ca>
2017-10-09 18:44:13 +0000

    created requestURL variable

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,13 @@
 var request = require('request');
+var GITHUB_USER = "rachaeltopp";
+var GITHUB_TOKEN = "b70c36951efd7bf53c616a6e3498b724236a400a";
+
 
 //console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 
 };
 
