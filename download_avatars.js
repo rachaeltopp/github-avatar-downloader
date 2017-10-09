94bb276a5da33cba0487b4c5182144bb111352a1
Rachael Topp <topp@ualberta.ca>
2017-10-09 20:08:33 +0000

    made get request and added user-agent header

--- download_avatars.js
+++ download_avatars.js
@@ -6,9 +6,13 @@ var GITHUB_TOKEN = "b70c36951efd7bf53c616a6e3498b724236a400a";
 //console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-
+  var requestURL = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'}
+  };
+  request.get(requestURL, function (error, response, body) {
+    console.log(body);
+  });
 };
 
 getRepoContributors("jquery", "jquery", function(err, result) {
